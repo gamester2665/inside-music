b4a7ef9f162e4f1bbd5b73fbb411c067a6780edf
gamester2665 <gamester2665@gmail.com>
2022-05-13 21:43:24 -0700

    working

--- audio/stems/split.py
+++ audio/stems/split.py
@@ -6,35 +6,86 @@ import random
 import string
 import sys
 
+
 def tmp_wav():
-    return (''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))) + ".wav"
+    return (
+        "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+    ) + ".wav"
+
 
 # split the track up in 30 second segments
 def split_track(folder, track_name, out_path):
     segment = 0
-    print folder, track_name
+    # print(folder, track_name)
     # normalize the wave file
     tmp_audio = tmp_wav()
-    os.system("ffmpeg -i %s/%s.wav -sample_fmt s16 -ac 1 -sample_rate 44100 %s" % (folder, track_name, tmp_audio))
+
+    # print(tmp_audio)
+    os.system(
+        "ffmpeg -i %s/%s.wav -sample_fmt s16 -ac 1 -sample_rate 44100 %s"
+        % (folder, track_name, tmp_audio)
+    )
     track = AudioSegment.from_wav(tmp_audio)
     while segment * 30 < track.duration_seconds:
-        seg_audio = track[segment*30000:(segment+1)*30005]
+        seg_audio = track[segment * 30000 : (segment + 1) * 30005]
         tmp = tmp_wav()
         seg_audio.fade_out(5).export(tmp, format="wav")
+
+        print(tmp)
+
         # convert the sample rate and channel numbers
-        # os.system("ffmpeg -y -i %s -codec:a libmp3lame -q:a 4 %s/%s-%d.mp3 " % (tmp, out_path, track_name, segment))
-        # os.system("ffmpeg -y -i %s -codec:a vorbis -aq 50 %s/%s-%d.mp3 " % (tmp, out_path, track_name, segment))
-        os.system("xld -o %s/%s-%d.mp3 --samplerate=44100 -f mp3 --channels=1 --correct-30samples %s" % (out_path, track_name, segment, tmp))
-        os.system("xld -o %s/%s-%d.ogg --samplerate=44100 -f vorbis --channels=1 --correct-30samples %s" % (out_path, track_name, segment, tmp))
+
+        # mp3Filename = f"{folder}/{track_name}-{segment}"
+        # mp3WithExt = mp3Filename.upper() + ".mp3"
+        # seg_audio.fade_out(5).export(mp3WithExt, format="mp3", parameters=["-ac", "2"])
+
+        # os.system(
+        #     "ffmpeg -y -i %s -codec:a libmp3lame -ac 1 -q:a 4 %s/%s-%d.mp3"
+        #     % (tmp, folder, track_name, segment)
+        # )
+
+        # copyToFolder(tmp, out_path, track_name, segment, "mp3")
+
+        # oggFilename = f"{folder}/{track_name}-{segment}"
+        # oggWithExt = oggFilename.upper() + ".ogg"
+        # seg_audio.fade_out(5).export(oggWithExt, format="ogg", parameters=["-ac", "2"])
+
+        os.system(
+            "ffmpeg -y -i %s -codec:a libvorbis -strict -2 -ac 1 -aq 50 %s/%s-%d.ogg"
+            % (tmp, out_path, track_name, segment)
+        )
+
+        # copyToFolder(tmp, out_path, track_name, segment, "ogg")
+
+        # os.system(
+        #     "xld -o %s/%s-%d.mp3 --samplerate=44100 -f mp3 --channels=1 --correct-30samples %s"
+        #     % (out_path, track_name, segment, tmp)
+        # )
+        # os.system(
+        #     "xld -o %s/%s-%d.ogg --samplerate=44100 -f vorbis --channels=1 --correct-30samples %s"
+        #     % (out_path, track_name, segment, tmp)
+        # )
+
         os.remove(tmp)
-        segment+=1
-        print track_name, segment
+        segment += 1
+        print(track_name, segment)
     os.remove(tmp_audio)
+
+
+def copyToFolder(tmp, folder, track_name, segment, ext):
+    print(tmp, folder, track_name, segment, ext)
+    import shutil
+
+    # make a copy of the invoice to work with
+    src = tmp
+    dst = f"{folder}/{track_name}-{segment}.{ext}"
+    shutil.copy(src, dst)
+
 
 # go through and run the split track on each track
 def split_song(folder):
     out_path = "../%s" % (folder)
-    print folder
+    print(folder)
     if not os.path.exists(out_path):
         os.makedirs(out_path)
     # get all of the tracks in the folder
@@ -42,6 +93,13 @@ def split_song(folder):
         if file.endswith(".wav"):
             split_track(folder, os.path.splitext(file)[0], out_path)
 
+
 if __name__ == "__main__":
     split_song(sys.argv[1])
+    # try:
+    #     split_song(sys.argv[1])
+    #     pass
+    # except Exception as e:
+    #     print(e)
+    #     raise e
 
